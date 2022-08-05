5482bd9856193bf0554f5a2d64866248a719f07a
prestonevans <89256614+prestonevans@users.noreply.github.com>
2022-08-04 23:31:41 -0600

    new idea for a unique menu

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
-//document.body.style.backgroundColor = "red";
-//test me
+const open = document.querySelector('.open')
+const close = document.querySelector('.close')
+const menu = document.querySelector('.menu')
+const circleNav = document.querySelector('.circle')
+
+open.addEventListener('click',() => {
+    circleNav.style.transform = 'rotate(-90deg)'
+    menu.style.transform = 'translateX(0)'
+})
+
+close.addEventListener('click',() => {
+    circleNav.style.transform = ''
+    menu.style.transform = 'translateX(-15vw)'
+
+})
+
